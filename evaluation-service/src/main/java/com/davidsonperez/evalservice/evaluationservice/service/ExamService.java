3917ee03980cdd7e73a91247f00eceee1740cee2
Davidson Perez <trosquinalidad132@gmail.com>
2023-04-17 15:42:05 -0500

    avance en service y controller

--- evaluation-service/src/main/java/com/davidsonperez/evalservice/evaluationservice/service/ExamService.java
+++ evaluation-service/src/main/java/com/davidsonperez/evalservice/evaluationservice/service/ExamService.java
@@ -1,19 +1,33 @@
 package com.davidsonperez.evalservice.evaluationservice.service;
 
-//import org.modelmapper.ModelMapper;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.davidsonperez.evalservice.evaluationservice.data.entity.Exam;
+import com.davidsonperez.evalservice.evaluationservice.data.entity.Option;
+import com.davidsonperez.evalservice.evaluationservice.data.entity.Question;
 import com.davidsonperez.evalservice.evaluationservice.data.repository.ExamRepository;
+import com.davidsonperez.evalservice.evaluationservice.data.repository.OptionRepository;
+import com.davidsonperez.evalservice.evaluationservice.data.repository.QuestionRepository;
 import com.davidsonperez.evalservice.evaluationservice.web.dto.ExamDto;
+import com.davidsonperez.evalservice.evaluationservice.web.dto.OptionDto;
+import com.davidsonperez.evalservice.evaluationservice.web.dto.QuestionDto;
 import com.davidsonperez.evalservice.evaluationservice.web.mapper.ExamMapper;
+import com.davidsonperez.evalservice.evaluationservice.web.mapper.OptionMapper;
+import com.davidsonperez.evalservice.evaluationservice.web.mapper.QuestionMapper;
 
 @Service
 public class ExamService {
     private ExamRepository examRepository;
+    private OptionRepository optionRepository;
+    private QuestionRepository questionRepository;
 
-    public ExamService(ExamRepository examRepository) {
+    public ExamService(ExamRepository examRepository, OptionRepository optionRepository, 
+    QuestionRepository questionRepository) {
         this.examRepository = examRepository;
+        this.optionRepository = optionRepository;
+        this.questionRepository = questionRepository;
     }
 
     public ExamDto saveExam(ExamDto examDto) throws Exception {
@@ -24,5 +38,33 @@ public class ExamService {
         Exam eExam = ExamMapper.INSTANCE.examDtoToExam(examDto);
         eExam = examRepository.save(eExam);
         return ExamMapper.INSTANCE.examToExamDto(eExam);
+    }
+
+    public List<QuestionDto> saveQuestions(List<QuestionDto> questionDtos) throws Exception {
+        List<Question> questions = QuestionMapper.INSTANCE.questionDtosToQuestions(questionDtos);
+
+        for (int i = 0; i < questions.size(); i++) {
+            if (questions.get(i) == null) {
+                throw new Exception("Parámetro no válido");
+            }
+
+            questionRepository.save(questions.get(i));
+        }
+        
+        return QuestionMapper.INSTANCE.questionsToQuestionDtos(questions);
+    }
+
+    public List<OptionDto> saveOptions(List<OptionDto> optionDtos) throws Exception {
+        List<Option> options = OptionMapper.INSTANCE.optionDtosToOptions(optionDtos);
+
+        for (int i = 0; i < options.size(); i++) {
+            if (options.get(i) == null) {
+                throw new Exception("Parámetro no válido");
+            }
+
+            optionRepository.save(options.get(i));
+        }
+
+        return OptionMapper.INSTANCE.optionsToOptionDtos(options);
     }
 }
--- evaluation-service/src/main/java/com/davidsonperez/evalservice/evaluationservice/web/controller/ExamController.java
+++ evaluation-service/src/main/java/com/davidsonperez/evalservice/evaluationservice/web/controller/ExamController.java
@@ -1,5 +1,7 @@
 package com.davidsonperez.evalservice.evaluationservice.web.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.davidsonperez.evalservice.evaluationservice.service.ExamService;
 import com.davidsonperez.evalservice.evaluationservice.web.dto.ExamDto;
+import com.davidsonperez.evalservice.evaluationservice.web.dto.OptionDto;
+import com.davidsonperez.evalservice.evaluationservice.web.dto.QuestionDto;
 
 @RestController
 @RequestMapping("exam")
@@ -30,6 +34,43 @@ public class ExamController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(resp);
+    }
+
+    @PostMapping("/saveQuestions")
+    public ResponseEntity<?> insertQuestions(@RequestBody List<QuestionDto> questionDtos) throws Exception {
+        for (int i = 0; i < questionDtos.size(); i++) {
+            if (questionDtos.get(i) == null) {
+                return ResponseEntity.badRequest().body("Datos de la pregunta inválidos");
+            }
+        }
+
+        List<QuestionDto> resp;
+        try {
+            resp = examService.saveQuestions(questionDtos);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(resp);
+    }
+
+    @PostMapping("/saveOptions")
+    public ResponseEntity<?> insertOptions(@RequestBody List<OptionDto> optionDtos) throws Exception {
+        for (int i = 0; i < optionDtos.size(); i++) {
+            if (optionDtos.get(i) == null) {
+                return ResponseEntity.badRequest().body("Datos de la pregunta inválidos");
+            }
+        }
+
+        List<OptionDto> resp;
+        try {
+            resp = examService.saveOptions(optionDtos);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
         return ResponseEntity.status(HttpStatus.CREATED).body(resp);
     }
 }
